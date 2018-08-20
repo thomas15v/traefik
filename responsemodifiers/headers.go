829e709323c3ebc9fd08ad03a241c5ea2f4da632
Daniel Tomcej <daniel.tomcej@gmail.com>
2018-08-20 16:51:34 -0600

    initial commit start cors implementation add cors logic validate fix unset origin create new headers handlers use header struct remove old config cleanup and responsemodifier more cleanup add simple cors tests add more preflights add new tests for header objects remove err on getorigin add labels to parser Add response tests add vary header update labels

--- responsemodifiers/headers.go
+++ responsemodifiers/headers.go
@@ -4,46 +4,43 @@ import (
 	"net/http"
 
 	"github.com/containous/traefik/config"
+	"github.com/containous/traefik/middlewares/headers"
 	"github.com/unrolled/secure"
 )
 
-func buildHeaders(headers *config.Headers) func(*http.Response) error {
+func buildHeaders(h *config.Headers) func(*http.Response) error {
 	opt := secure.Options{
-		BrowserXssFilter:        headers.BrowserXSSFilter,
-		ContentTypeNosniff:      headers.ContentTypeNosniff,
-		ForceSTSHeader:          headers.ForceSTSHeader,
-		FrameDeny:               headers.FrameDeny,
-		IsDevelopment:           headers.IsDevelopment,
-		SSLRedirect:             headers.SSLRedirect,
-		SSLForceHost:            headers.SSLForceHost,
-		SSLTemporaryRedirect:    headers.SSLTemporaryRedirect,
-		STSIncludeSubdomains:    headers.STSIncludeSubdomains,
-		STSPreload:              headers.STSPreload,
-		ContentSecurityPolicy:   headers.ContentSecurityPolicy,
-		CustomBrowserXssValue:   headers.CustomBrowserXSSValue,
-		CustomFrameOptionsValue: headers.CustomFrameOptionsValue,
-		PublicKey:               headers.PublicKey,
-		ReferrerPolicy:          headers.ReferrerPolicy,
-		SSLHost:                 headers.SSLHost,
-		AllowedHosts:            headers.AllowedHosts,
-		HostsProxyHeaders:       headers.HostsProxyHeaders,
-		SSLProxyHeaders:         headers.SSLProxyHeaders,
-		STSSeconds:              headers.STSSeconds,
+		BrowserXssFilter:        h.BrowserXSSFilter,
+		ContentTypeNosniff:      h.ContentTypeNosniff,
+		ForceSTSHeader:          h.ForceSTSHeader,
+		FrameDeny:               h.FrameDeny,
+		IsDevelopment:           h.IsDevelopment,
+		SSLRedirect:             h.SSLRedirect,
+		SSLForceHost:            h.SSLForceHost,
+		SSLTemporaryRedirect:    h.SSLTemporaryRedirect,
+		STSIncludeSubdomains:    h.STSIncludeSubdomains,
+		STSPreload:              h.STSPreload,
+		ContentSecurityPolicy:   h.ContentSecurityPolicy,
+		CustomBrowserXssValue:   h.CustomBrowserXSSValue,
+		CustomFrameOptionsValue: h.CustomFrameOptionsValue,
+		PublicKey:               h.PublicKey,
+		ReferrerPolicy:          h.ReferrerPolicy,
+		SSLHost:                 h.SSLHost,
+		AllowedHosts:            h.AllowedHosts,
+		HostsProxyHeaders:       h.HostsProxyHeaders,
+		SSLProxyHeaders:         h.SSLProxyHeaders,
+		STSSeconds:              h.STSSeconds,
 	}
 
 	return func(resp *http.Response) error {
-		if headers.HasCustomHeadersDefined() {
-			// Loop through Custom response headers
-			for header, value := range headers.CustomResponseHeaders {
-				if value == "" {
-					resp.Header.Del(header)
-				} else {
-					resp.Header.Set(header, value)
-				}
+		if h.HasCustomHeadersDefined() || h.HasCorsHeadersDefined() {
+			err := headers.NewHeader(nil, *h).ModifyResponseHeaders(resp)
+			if err != nil {
+				return err
 			}
 		}
 
-		if headers.HasSecureHeadersDefined() {
+		if h.HasSecureHeadersDefined() {
 			err := secure.New(opt).ModifyResponseHeaders(resp)
 			if err != nil {
 				return err
